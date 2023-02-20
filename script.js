761a460954c9a30d9a4cd22a57966af355745842
Usman Mushtaq <mushtusm@corp.contoursoftware.net>
2023-02-20 12:43:14 +0500

    form-validation

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const btnSubmit=document.getElementById('addCon');
+const btnSubmit = document.getElementById('addCon');
 btnSubmit.addEventListener('click', myfunc);
 
- function myfunc(){
-    const name = document.getElementById('formName').value;
-    const email = document.getElementById('formEmail').value;
-    const phone = document.getElementById('formPassword').value;
-    const type = document.getElementsByName('type');
-    const cards = document.getElementById('cards');
+function myfunc() {
+  const name = document.getElementById('formName').value;
+  const email = document.getElementById('formEmail').value;
+  const phone = document.getElementById('formPassword').value;
+  const type = document.getElementsByName('type');
+  const cards = document.getElementById('cards');
 
-    for(let i=0;i<type.length;i++){
-        if(type[i].checked){
-            var profession = type[i].value;
-        }
+  for (let i = 0; i < type.length; i++) {
+    if (type[i].checked) {
+      var profession = type[i].value;
     }
+  }
 
-     const classname = profession === 'Personal' ? 'btn-primary' : 'btn-success'
+  if (!validateForm(name, email, phone)) {
+    return false;
+  }
 
-        const card = document.createElement('div');
-        card.classList='card';
-        const cardInner= `<section class="card d-flex flex-row crd col-lg-12">
+  const classname = profession === 'Personal' ? 'btn-primary' : 'btn-success'
+
+  const card = document.createElement('div');
+  card.classList = 'card';
+  const cardInner = `<section class="card d-flex flex-row crd col-lg-12">
         <div class="cardLeft col-lg-9">
           <h2 class="userName">${name}</h2>
           <p class="userMail"><i class="fa-solid fa-envelope-open userIcon Icon"></i>${email}</p>
@@ -33,9 +37,32 @@ btnSubmit.addEventListener('click', myfunc);
           <img src="img/img_avatar1.png" alt="ishan" width="150px" height="150px">
         </div>
       </section>`;
-        
-        card.innerHTML += cardInner;
-        cards.appendChild(card);
-    }
+
+  card.innerHTML += cardInner;
+  cards.appendChild(card);
+}
 
 
+function validateForm(name, email, phone){
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  if (!/(\W|^)[\w.+\-]*@gmail\.com(\W|$)/.test(email)){
+    alert("You have entered an invalid email address!")
+    return false;
+  }
+  if (phone == "") {
+    alert("Phone must be filled out");
+    return false;
+  }
+  if (!/^((\+92)|(0092))-{0,1}\d{3}-{0,1}\d{7}$|^\d{11}$|^\d{4}-\d{7}$/.test(phone)){
+    alert("You have entered an invalid phone number!")
+    return false;
+  }
+  return true;
+}
